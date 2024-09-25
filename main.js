2fc8c39290ad9a2b7072a7605f8b4105674f6332
mynewhome25@gmail.com <mynewhome25@gmail.com>
2024-09-25 19:40:09 +0800

    <新增>(index): 新增首頁新手指南與熱門商家區塊，以及footer與navbar

--- main.js
+++ main.js
@@ -3,5 +3,39 @@ import 'bootstrap';
 // swiper 這段 import 請不要動它，因為還沒寫輪播，所以會顯示錯誤
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
 
+//index
+//熱門商家
+var swiper = new Swiper(".index-featuredShopSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 24,
+  centeredSlides: true,
+  
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1.25,
+      spaceBetween: 24,
+      centeredSlides: true,
+    },
+    540: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+      centeredSlides: false,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+      centeredSlides: false,
+    },
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 console.log("Hello world!");
