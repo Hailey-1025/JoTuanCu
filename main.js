9b1af987a67962f412eb2570b13bcc8528b6b3f4
倫倫 <f5badapple@gmail.com>
2024-10-01 21:02:15 +0800

    <新增>(groupActivities)新增揪團活動頁面，選擇日期的input還在調整中

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-import './assets/scss/all.scss';
-import 'bootstrap';
+import "./assets/scss/all.scss";
+import "bootstrap";
 // swiper 這段 import 請不要動它，因為還沒寫輪播，所以會顯示錯誤
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
 
+// const customIcon = document.querySelector(".custom-date-icon");
+// const dateInput = document.getElementById("dateInput");
 
-console.log("Hello world!");
+// // 点击自定义图标时，触发日期选择器
+// customIcon.addEventListener("click", function () {
+//   dateInput.click(); // 触发日期输入框的点击效果
+//   console.log("Date input focused"); // 確認是否觸發
+// });
+
+// const dateInput = document.getElementById("dateInput");
+// const customIcon = document.getElementById("customIcon");
+
+// customIcon.addEventListener("click", function () {
+//   dateInput.focus(); // 聚焦日期輸入框
+//   dateInput.click(); // 觸發日期選擇器
+// });
