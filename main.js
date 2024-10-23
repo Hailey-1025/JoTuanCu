6a49508e1b317a5ce6d925cc987f554db4a0f0b7
mynewhome25@gmail.com <mynewhome25@gmail.com>
2024-10-23 19:53:31 +0800

    <修改>增加首頁動畫

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import "./assets/scss/all.scss";
 import "bootstrap";
 // swiper 這段 import 請不要動它，因為還沒寫輪播，所以會顯示錯誤
 import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
+//wow
+new WOW().init();
 
 //index
 //最新活動
